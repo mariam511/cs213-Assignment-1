043fd0e5cc619ca2bed6e9d44d6987ac2fa01cba
josiane usama <147441305+JosianeUsama@users.noreply.github.com>
2023-10-18 12:09:46 +0300

    Update assignment 1.cpp

--- assignment 1.cpp
+++ assignment 1.cpp
@@ -1,4 +1,4 @@
-// Program: assignment1 part1
+CS213-2023-20220502-20220520-20221141-A1-Part1.cpp
 // Purpose: Demonstrate use of bmplip for handling
 //          bmp colored and grayscale images
 //          Program load a gray image and store in another file
